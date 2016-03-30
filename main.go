0468d028fa4a048a9b9c6e53b8b76628c0b8267d
ehernandez <ehernandez@xumak.com>
2016-03-30 16:36:16 -0600

    Removed unnecessary comments.

--- main.go
+++ main.go
@@ -15,16 +15,4 @@ func main() {
 	//ListenAndServe starts an HTTP server with a given address and handler
 	log.Fatal(http.ListenAndServe(":8080", router))
 
-	// run using: go run *.go
-
-	//Using curl instead the browser, in other terminal
-
-	// curl -H "Content-Type: application/json" -d '{"name":"New Todo"}' http://localhost:8080/todos
-	// shows: {"id":5,"name":"New Todo","completed":false,"due":"0001-01-01T00:00:00Z"}
-
-	// view all todos
-	// curl http://localhost:8080/todos
-
-	// find a todo http://localhost:8080/todos/3
-
 }
