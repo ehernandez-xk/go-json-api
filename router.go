93e70dcb757f47fccaf2f5dad835bed4677b6562
ehernandez <ehernandez@xumak.com>
2016-03-30 10:19:00 -0600

    8 - Refactor routes.go and separated logic to router.go

---
+++ router.go
@@ -0,0 +1,36 @@
+package main
+
+import "net/http"
+
+//Route is the struct of each route
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+//Routes is a slice of Route
+type Routes []Route
+
+//The definition of routes.
+var routes = Routes{
+	Route{
+		"Index",
+		"GET",
+		"/",
+		Index,
+	},
+	Route{
+		"TodoIndex",
+		"GET",
+		"/todos",
+		TodoIndex,
+	},
+	Route{
+		"TodoShow",
+		"GET",
+		"/todos/{todoId}",
+		TodoShow,
+	},
+}
--- routes.go
+++ routes.go
@@ -6,39 +6,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-//Route is the struct of each route
-type Route struct {
-	Name        string
-	Method      string
-	Pattern     string
-	HandlerFunc http.HandlerFunc
-}
-
-//Routes is a slice of Route
-type Routes []Route
-
-//The definition of routes.
-var routes = Routes{
-	Route{
-		"Index",
-		"GET",
-		"/",
-		Index,
-	},
-	Route{
-		"TodoIndex",
-		"GET",
-		"/todos",
-		TodoIndex,
-	},
-	Route{
-		"TodoShow",
-		"GET",
-		"/todos/{todoId}",
-		TodoShow,
-	},
-}
-
 //NewRouter returns a router that cointains the handlers
 func NewRouter() *mux.Router {
 
