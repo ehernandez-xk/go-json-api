ae09af05af6b79c59ee6299792bae81da1346637
ehernandez <ehernandez@xumak.com>
2016-03-30 06:17:39 -0600

    2 - Creating some basic routes. TodoIndex and TodoShow

--- api.go
+++ api.go
@@ -16,14 +16,35 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
 
+//TodoIndex the index of Todo
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Todo Index!")
+}
+
+//TodoShow Show todos
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoID := vars["todoId"]
+	fmt.Fprintln(w, "Todo show: ", todoID)
+}
+
 func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
 	//Using router instead of http.HandlFunc, This router only works to localhost:8080/
 	router.HandleFunc("/", Index)
+	router.HandleFunc("/todos", TodoIndex)
+
+	//we added a variable in the route, called todoId
+	router.HandleFunc("/todos/{todoId}", TodoShow)
+
 	log.Fatal(http.ListenAndServe(":8080", router))
 
 	// When runs: http://localhost:8080/test/test
 	// comes: 404 page not found
+	// When runs:  http://localhost:8080/todos
+	// comes: Todo Index!
+	// When runs:  http://localhost:8080/todos/myTodo
+	// comes: Todo show:  myTodo
 
 }
